38bdf950362f5401acae8b2a1dc0399d71c67dc5
SURAJ <republicofgamers.srg@gmail.com>
2022-02-14 13:42:56 +0530

    added heroku server env variables

---
+++ config/env/production/database.js
@@ -0,0 +1,13 @@
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'mysql',
+    connection: {
+      host: env('DATABASE_HOST', '144.126.252.122'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME', 'iiiflnext'),
+      user: env('DATABASE_USERNAME', 'iifl'),
+      password: env('DATABASE_PASSWORD', 'Ritesh@123'),
+      ssl: env.bool('DATABASE_SSL', false),
+    },
+  },
+});
